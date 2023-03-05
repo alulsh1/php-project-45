96ed3b516ce7afec3e22454f3bcc5c93687ecf32
Aleksandr Ulshin <aleksandr.ulshin@rhsolutions.local>
2023-03-05 18:03:56 +0300

    'asd'

--- src/Games/BrainPprogression.php
+++ src/Games/BrainPprogression.php
@@ -1,37 +1,36 @@
 <?php
 namespace BrainGames\Games\BrainPprogression;
-use function BrainGames\Engine\runner;
-use const BrainGames\Engine\ROUND_COUNT;
+use function BrainGames \Engine \runner;
+use const BrainGames \Engine \ROUND_COUNT;
 
-function progression(){
+function progression()
+{
+	$dlinaProgress = rand(5, 10);
+	$startNum = rand(1, 10);
+	$razd = rand(2, 10);
 
-$dlinaProgress = rand(5, 10);
-$startNum = rand(1, 10);
-$razd = rand(2, 10);
+	$arefProgress = [];
 
-$arefProgress = [];
-
-for ($i=0; $i <$dlinaProgress; $i++){
-	$arefProgress[] = $startNum;
-	$startNum = $startNum + $razd;
+	for ($i = 0; $i < $dlinaProgress; $i++) {
+		$arefProgress[] = $startNum;
+		$startNum = $startNum + $razd;
+	}
+	return $arefProgress;
 }
-return $arefProgress;
-}
 
-function run(){
-$description = 'What number is missing in the progression?';
+function run()
+{
+	$description = 'What number is missing in the progression?';
 
-for ($i=0; $i <3; $i++){
-		
-	$arr = progression();
-	
-	$randonNum = rand(0, count($arr)-1);
-	$arrWork[$i]['result'] = $arr[$randonNum];
-	
-	$arr[$randonNum] = "..";
+	for ($i = 0; $i < 3; $i++) {
+		$arr = progression();
 
-	$arrWork[$i]['question'] = implode(" ", $arr);
+		$randonNum = rand(0, count($arr) - 1);
+		$arrWork[$i]['result'] = $arr[$randonNum];
 
-}
+		$arr[$randonNum] = "..";
+
+		$arrWork[$i]['question'] = implode(" ", $arr);
+	}
 	runner($description, $arrWork);
 }
